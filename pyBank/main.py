0db7152a9d938d2507552bb884f26b0e04f4dbe2
Younes1120 <126006692+Younes1120@users.noreply.github.com>
2023-03-18 23:17:48 -0600

    budget_data_solved

---
+++ pyBank/main.py
@@ -0,0 +1,62 @@
+import os
+import csv
+
+
+budget_data_csv = os.path.join("budget_data.csv")
+
+
+text_path = "output.txt"
+
+
+total_months = 0
+total_revenue = 0
+revenue = []
+previous_revenue = 0
+month_of_change = []
+revenue_change = 0
+greatest_decrease = ["", 9999999]
+greatest_increase = ["", 0]
+revenue_change_list = []
+revenue_average = 0
+
+
+
+with open('budget_data.csv') as csvfile:  
+    csvreader = csv.DictReader(csvfile)
+
+   
+    for row in csvreader:
+
+     
+        total_months += 1
+
+      
+        total_revenue = total_revenue + int(row["Profit/Losses"])
+
+       
+        revenue_change = float(row["Profit/Losses"])- previous_revenue
+        previous_revenue = float(row["Profit/Losses"])
+        revenue_change_list = revenue_change_list + [revenue_change]
+        month_of_change = [month_of_change] + [row["Date"]]
+       
+
+    
+        if revenue_change>greatest_increase[1]:
+            greatest_increase[1]= revenue_change
+            greatest_increase[0] = row['Date']
+
+      
+        if revenue_change<greatest_decrease[1]:
+            greatest_decrease[1]= revenue_change
+            greatest_decrease[0] = row['Date']
+    revenue_average = sum(revenue_change_list)/len(revenue_change_list)
+
+
+with open(text_path, 'w') as file:
+    file.write("Financial Analysis\n")
+    file.write("---------------------\n")
+    file.write("Total Months: %d\n" % total_months)
+    file.write("Total Revenue: $%d\n" % total_revenue)
+    file.write("Average Revenue Change $%d\n" % revenue_average)
+    file.write("Greatest Increase in Revenue: %s ($%s)\n" % (greatest_increase[0], greatest_increase[1]))
+    file.write("Greatest Decrease in Revenue: %s ($%s)\n" % (greatest_decrease[0], greatest_decrease[1]))
